9aa5e265f8308d942569832a16729953d0063488
muhammadfaisal1194 <muhammadfaisal1194@gmail.com>
2023-01-31 20:28:44 +0500

    update

--- src/api/portfolio-categorie/routes/portfolio-categorie.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * portfolio-categorie router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::portfolio-categorie.portfolio-categorie');
